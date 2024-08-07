04f62b3d856ff980befb84388144f56cf70e9792
faizan-rauf05 <faizanrauf2525@gmail.com>
2024-08-07 23:41:34 +0500

    final commit-1

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.post("/webhook", bodyParser.raw({ type: "application/json" }), afterStripe);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 // app.use(cors());
 app.use(express.static("public"));
 
