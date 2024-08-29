3d5e96935b54ad4c4e05dd0f218d4d75ae18a836
faizan-rauf05 <faizanrauf2525@gmail.com>
2024-08-30 00:40:59 +0500

    remove options cors

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ import cookieParser from "cookie-parser";
 import AdminRoutes from "./routes/AdminRoutes.js";
 import CompanyRoutes from "./routes/companyRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import { stripePayment } from "./controllers/stripeController.js";
+import { stripePayment, afterStripe } from "./controllers/stripeController.js";
 import bodyParser from "body-parser";
-import { afterStripe } from "./controllers/stripeController.js";
 import { realtimeFlightsData } from "./controllers/FlightsController.js";
 
 dotenv.config();
 const app = express();
 
-// Databse Connection
+// Database Connection
 dbCon();
 
+// Webhook for Stripe
 app.post("/webhook", bodyParser.raw({ type: "application/json" }), afterStripe);
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS Configuration
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -29,12 +33,14 @@ app.use(cors({
   credentials: true 
 }));
 
+// Static Files
 app.use(express.static("public"));
 
-
-app.get("/", (req, res)=>{
+// Routes
+app.get("/", (req, res) => {
   res.send("Server is running");
-})
+});
+
 app.use("/api/auth", AuthRoutes);
 app.use("/api/admin", AdminRoutes);
 app.use("/api/company", CompanyRoutes);
@@ -42,15 +48,7 @@ app.use("/api/user", userRoutes);
 app.post("/api/create-checkout-session", stripePayment);
 app.get("/api/destinations", realtimeFlightsData);
 
-app.options('*', cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
 // Server Listening
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is runnning");
+  console.log("Server is running");
 });
-
